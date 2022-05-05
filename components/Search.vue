a69614abb914c1d6ae9c88229837fdeca5386fdb
Vlad <clay.zenx@gmail.com>
2022-05-05 11:33:46 +0700

    Typing

--- components/Search.vue
+++ components/Search.vue
@@ -1,15 +1,24 @@
 <script lang="ts" setup>
-const { inputValue } = defineProps<{
-  inputValue: string
+const { inputValue, selectedCategory } = defineProps<{
+  inputValue: string,
+  selectedCategory: IStrapiCategory
 }>()
 
-const emit = defineEmits(['update:inputValue'])
+const emit = defineEmits(['update:selectedCategory', 'update:inputValue'])
 const input = () => emit('update:inputValue', inputValue)
+const close = () => emit('update:selectedCategory', null)
 
 </script>
 
 <template>
-  <div flex gap-2 p-2 items-center justify-left text-right>
+  <div flex gap-2 p-2 items-center justify-left text-right all:transition-400>
+    <span v-if="selectedCategory" flex items-center gap-1 rounded py-1 px-2 text-sm bg-dark op70 cursor-pointer
+      hover="bg-dark-200" @click="close">
+      <span>
+        {{ selectedCategory?.attributes.name }}
+      </span>
+      <div i-ion-close-outline />
+    </span>
     <input type="text" v-model="inputValue" @input="input" bg-transparent p-2 text-lg focus:outline-none
       placeholder="Search something..." />
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,22 @@
 <script lang="ts" setup>
+import { Strapi4Response } from '@nuxtjs/strapi/dist/runtime/types';
 const selectedCategory = useState<IStrapiCategory>('selectedCategory')
 const searchEntry = useState<string>('searchEntry')
 const { find } = useStrapi4()
 const { result, search } = useSearch('production_api::article.article')
-const { data: categories } = await useAsyncData('categories', () => find('categories'))
+const { data: categories } = await useAsyncData('categories', () => find<Strapi4Response<IStrapiCategory>>('categories'))
 const searchPosts = () => search({ query: searchEntry.value })
 const searchHits = computed(() =>
   selectedCategory.value ? result.value.hits.filter(hit => hit.category.id === selectedCategory.value.id) : result.value.hits
 )
+onMounted(() => search({}))
 watch(searchEntry, searchPosts)
 </script>
 
 <template>
   <div>
-    <Search v-model:inputValue="searchEntry" />
-    <div>{{ selectedCategory?.attributes.name }}</div>
-
-    <Categories v-model:selected="selectedCategory" :categories="categories.data" />
+    <Search v-model:inputValue="searchEntry" v-model:selectedCategory="selectedCategory" />
+    <Categories v-model:selected="selectedCategory" :categories="categories.data as Array<IStrapiCategory>" />
     <pre v-if="result">{{ searchHits }}</pre>
-
   </div>
 </template>
