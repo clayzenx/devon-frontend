d3f1006a5f3c3360a279dbb0869e4b576e514521
Vlad <clay.zenx@gmail.com>
2022-05-07 10:45:09 +0700

    Logo

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header ma mt-10 max-w-4xl flex justify-between>
-    <h2>Devon_</h2>
+    <h2>devon<span animate-pulse>_</span> </h2>
     <section flex gap4 align-center>
       <Locale />
       <DarkToggle />
